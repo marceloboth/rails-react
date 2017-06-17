c923da4e2b26531d5c195b514dd3356c6be0b50f
Marcelo J. Both <marcelo.both@gmail.com>
2017-06-17 18:42:35 -0300

    Implemented image upload at the model.

--- backend-api/app/models/field.rb
+++ backend-api/app/models/field.rb
@@ -1,2 +1,3 @@
 class Field < ApplicationRecord
+  mount_base64_uploader :image, ImageUploader
 end
--- backend-api/db/schema.rb
+++ backend-api/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615140617) do
+ActiveRecord::Schema.define(version: 20170617023022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "fields", force: :cascade do |t|
     t.string "name"
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
